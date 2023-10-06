d1a201a8ddc2371173da522fd496eb2c4b66e8e7
Ana <anabeatriz_112@outlook.com>
2023-10-05 23:41:02 -0300

    integração com a API do GitHub

--- script.js
+++ script.js
@@ -46,3 +46,46 @@ btnSearch.addEventListener('click', ()=> {
 })
 
 // integração com API
+const form = document.getElementById("search-bar")
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const search = document.getElementById("input").value
+
+    const originalName = search.split(' ').join('')
+    
+    document.getElementById("section-1").innerHTML = ""
+
+    fetch("https://api.github.com/users/"+originalName)
+    .then((result) => result.json())
+    .then((data) => {
+        document.getElementById("section-1").innerHTML = `
+            <img src="${data.avatar_url}"/>
+        `
+
+        document.getElementById("names-profile").innerHTML = `
+            <h1>${data.name}</>
+            <a target="_blank" href="https://www.github.com/${originalName}"><p id="username">@${data.login}</p></a>
+        `
+        document.getElementById("bio-description").innerHTML = `
+            <p id="bio-description">${data.bio}</p>
+        `
+        document.getElementById("public-repositories").innerHTML = `
+            <span>Public Repositories: ${data.public_repos}</span>
+        `
+        document.getElementById("followers").innerHTML = `
+            <span>Followers: ${data.followers}</span>
+        `
+        document.getElementById("following").innerHTML = `
+            <span>Following: ${data.following}</span>
+        `
+
+        document.getElementById("projects").innerHTML = `
+            <span><img src="./assets/icon-book.svg"><span>
+        `
+    })
+    .catch(error => {
+        console.error("Erro ao buscar informações do usuário do GitHub:", error);
+    });
+})
